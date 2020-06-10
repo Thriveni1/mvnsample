d0a820bb0d9cd628a8262588958e8707cb521d59
syntel <syntel@syncgdc3947.syntelorg.com>
2020-06-10 09:59:22 +0530

    commited myDemoMaven

---
+++ src/main/java/com/atos/MyDemoMaven/App.java
@@ -0,0 +1,20 @@
+package com.atos.MyDemoMaven;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+ApplicationContext ctx=null;
+ctx=new ClassPathXmlApplicationContext("a.xml");
+
+System.out.println(ctx.getBean("emp"));
+
+    }
+}
